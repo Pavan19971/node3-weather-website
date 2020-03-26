02a70e743dbc8dea41a2bc6ac4793c802186105d
Pavan Sista <pavansista97@gmail.com>
2020-03-26 19:27:02 -0400

    setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -1,18 +1,14 @@
-console.log("client side javascrpit loaded")
-
-
-
-
 const weatherForm = document.querySelector('form')
 const searchTerm = document.querySelector('input')
 const messageOne = document.getElementById('message-1')
- messageTwo = document.getElementById('message-2');
+var messageTwo = document.getElementById('message-2');
 
 weatherForm.addEventListener('submit',(e)=>{
     e.preventDefault()
     messageOne.textContent="Loading..."
     messageTwo.textContent=''
-    fetch('http://localhost:3000/weather?address='+searchTerm.value+'').then((response)=>{
+
+    fetch('/weather?address='+searchTerm.value+'').then((response)=>{
     response.json().then((data)=>{
         if(data.error){
             messageOne.textContent=data.error
